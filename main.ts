e6b52387b8d48d677d86b02a42fbd72513f916c3
whiteantchow <115450420+whiteantchow@users.noreply.github.com>
2022-10-17 14:42:55 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,52 @@ let changes = 0
 input.onButtonPressed(Button.A, function () {
     changes = randint(1, 6)
 })
+input.onButtonPressed(Button.AB, function () {
+    changes = 0
+})
 input.onButtonPressed(Button.B, function () {
-	
+    changes = 3
+    basic.showNumber(changes)
+    while (changes > 0) {
+        changes += -1
+        basic.showNumber(changes)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
 })
 basic.forever(function () {
-    basic.showNumber(changes)
+	
 })
